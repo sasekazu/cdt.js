cb906fe708149d30cc18fb59046c7bee3d9452d2
Kazuya Sase <sasekazu@gmail.com>
2015-02-13 23:24:59 +0900

    initial commit

---
+++ js/delaunay.js
@@ -0,0 +1,272 @@
+// JavaScript Document
+/// <reference path="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" />
+/// <reference path="numeric-1.2.6.min.js" />
+
+
+// ドロネー三角形分割用の三角形クラス
+// 引数 points 頂点の座標 [[x1,y1],[x2,y2],......]
+// 引数 indices 頂点のインデックス
+function DelauneyTriangle(points, indices){
+	this.adjacent=[null, null, null];	// 隣接する三角形の参照，辺12, 辺23，辺31において隣接する三角形を順に格納する
+	this.edgeIDinAdjacent=[-1,-1,-1];	// adjacentの各要素と対応．adjacent[i]が隣接する辺ID．辺IDはadjacent側のもの．	
+	this.vertexID=numeric.clone(indices);
+	this.init(points);
+	this.prev=null;	// 双方向連結リストの前ポインタ
+	this.next=null;	// 双方向連結リストの次ポインタ
+}
+
+DelauneyTriangle.prototype.push=function (next) {
+	this.next=next;
+	next.prev=this;
+}
+
+DelauneyTriangle.prototype.deleteAndAdd = function (newPointID, points) {
+
+	// 新しい三角形オブジェクトの作成
+	var indices;
+	var newTri=new Array(3);
+	for(var i = 0; i < 3; ++i) {
+		indices=[newPointID, this.vertexID[i], this.vertexID[(i+1)%3]];
+		newTri[i] = new DelauneyTriangle(points, indices);
+	}
+
+	// next の更新
+	if(this.prev != null) {
+		this.prev.next=newTri[0];
+	}
+	newTri[0].next=newTri[1];
+	newTri[1].next=newTri[2];
+	newTri[2].next=this.next;
+
+	// prev の更新
+	if(this.next != null) {
+		this.next.prev=newTri[2];
+	}
+	newTri[2].prev=newTri[1];
+	newTri[1].prev=newTri[0];
+	newTri[0].prev=this.prev;
+
+	// adjacent, edgeIDAdjacent の更新
+	// newPointを含む辺では他のnewTriと隣接
+	// newPointを含まない辺では,
+	// もともとの三角形の隣接三角形と隣接する
+	for(var i = 0; i < 3; ++i) {
+		newTri[i].adjacent[0]=newTri[(i+2)%3];
+		newTri[i].adjacent[1]=this.adjacent[i];
+		newTri[i].adjacent[2]=newTri[(i+1)%3];
+		newTri[i].edgeIDinAdjacent[0]=2;
+		newTri[i].edgeIDinAdjacent[1]=this.edgeIDinAdjacent[i];
+		newTri[i].edgeIDinAdjacent[2]=1;
+	}
+	
+	swapping(newPointID, newTri[0], points);
+	swapping(newPointID, newTri[1], points);
+	swapping(newPointID, newTri[2], points);
+
+	// スワッピングアルゴリズム
+	function swapping(newPointID, tri, points){
+		var newPtTri;	// newPointのtriにおけるローカルID
+		var oppEdgeTri;	// newPointの対辺のtriにおけるローカルID, opp: opposite
+		var adjTri;		// newPointの対辺で隣接する三角形
+		var oppEdgeAdj;	// 対辺のadjTriにおけるローカルID
+		var farPtAdj;	// triと共有しないadjTriの頂点のローカルID
+		var vOpp;		// 対辺のベクトル
+		var vFar;		// newPointとfarPointの相対ベクトル
+		for(var i = 0; i < 3; ++i) {
+			if(tri.vertexID[i] == newPointID) {
+				newPtTri=i;
+				oppEdgeTri=(newPtTri+1)%3
+				break;
+			}
+		}
+		adjTri = tri.adjacent[oppEdgeTri];
+		oppEdgeAdj = tri.edgeIDinAdjacent[oppEdgeTri];
+		farPtAdj = (oppEdgeAdj+2)%3;
+		vOpp = numeric.sub(points[tri.vertexID[(newPtTri+2)%3]], points[tri.vertexID[(newPtTri+1)%3]]);
+		vFar = numeric.sub(points[adjTri.vertexID[farPtAdj]], points[tri.vertexID[newPtTri]]);
+		// 対辺がvFarより短いときスワップ
+		if(numeric.norm2(vOpp) > numeric.norm2(vFar)) {
+
+			// vertexID の更新
+			tri.vertexID[(newPtTri+2)%3] = adjTri.vertexID[farPtAdj];
+			adjTri.vertexID[(farPtAdj+2)%3] = tri.vertexID[newPtTri];
+			// adjacent の更新
+			tri.adjacent[oppEdgeTri]=adjTri.adjacent[(oppEdgeAdj+1)%3];
+			adjTri.adjacent[oppEdgeAdj]=tri.adjacent[(oppEdgeTri+1)%3];
+			tri.adjacent[(oppEdgeTri+1)%3]=adjTri;
+			adjTri.adjacent[(oppEdgeAdj+1)%3]=tri;
+			// edgeIDinAdjacent の更新
+			tri.edgeIDinAdjacent[oppEdgeTri]=adjTri.edgeIDinAdjacent[(oppEdgeAdj+1)%3];
+			adjTri.edgeIDinAdjacent[oppEdgeAdj]=tri.edgeIDinAdjacent[(oppEdgeTri+1)%3];
+			tri.edgeIDinAdjacent[(oppEdgeTri+1)%3]=oppEdgeAdj;
+			adjTri.edgeIDinAdjacent[(oppEdgeAdj+1)%3]=oppEdgeTri;
+		}
+	}
+}
+
+
+DelauneyTriangle.prototype.init = function(points){
+	var v1=numeric.sub(points[this.vertexID[1]], points[this.vertexID[0]]);
+	var v2=numeric.sub(points[this.vertexID[2]], points[this.vertexID[0]]);
+	// もし，頂点が時計回りならばpoints[1]とpoints[2]を入れ替える
+	// v1 cross v2 のz座標が負であれば時計回り
+	var tmp;
+	if(v1[0]*v2[1]-v1[1]*v2[0]<0) {
+		tmp=this.vertexID[1];
+		this.vertexID[1]=this.vertexID[2];
+		this.vertexID[2]=tmp;
+	}
+}
+
+
+// ドロネー三角形分割関数
+// ※非常に効率の悪い実装
+// 引数1 inputPoints: 入力点の座標 [[x1,y1],[x2,y2],....]
+// 引数2,3,4,5 ymax, ymin, xmax, xmin: 入力点が含まれる領域の最大・最小座標
+// 返り値：triangleのコネクティビティ 
+// 例: [[0,1,2], [0,1,3], [1,2,4]..] は 
+// inputPoints[0], inputPoints[1], inputPoints[2] が三角形を構成する。
+// …以下、同様
+function DelaunayTriangulation(inputPoints, ymax, ymin, xmax, xmin) {
+
+	var pos = numeric.clone(inputPoints);	// 点の数 x 2(x,y)
+	var dPos = [];	// 動的に変わる点
+	var tri = [];	// 三角形の数 x 3(三角形頂点の点番号)
+
+	// すべての点を内包する
+	// 大きい三角形(superTriangle)の頂点を追加
+	// 下の点, 上の点1, 上の点2の順
+	dPos.push([(xmax+xmin)*0.5, ymin-(xmax-xmin)*0.5*1.73205080757]);
+	dPos.push([(xmax+xmin)*0.5-(xmax-xmin)*0.5-(ymax-ymin)/1.73205080757, ymax]);
+	dPos.push([(xmax+xmin)*0.5+(xmax-xmin)*0.5+(ymax-ymin)/1.73205080757, ymax]);
+	tri.push([0, 1, 2]);
+
+	// すべての入力点が追加されるまで実行
+	for(var step=0; step<pos.length; ++step) {
+
+		// 新しい入力点を追加
+		p = pos[step];
+		dPos.push(p);
+
+		// 入力点を外接円に内包する三角形を探す
+		// その三角形は削除対象として格納される
+		// このリストは値が降順になるように格納
+		// 注意：総当たりで調べている。非効率！
+		var removeTri=[];
+		for(var i=0; i<tri.length; i++) {
+			var c=new Circumcircle(
+							dPos[tri[i][0]],
+							dPos[tri[i][1]],
+							dPos[tri[i][2]]
+						);
+			var distVec=numeric.sub(c.p, p);
+			var dist=numeric.norm2(distVec);
+			if(dist<c.rad) {
+				removeTri.unshift(i);
+			}
+		}
+
+		// removeTriに含まれる三角形を削除してできる
+		// 多角形の頂点を抽出する
+		var pointsDupricated = [];
+		for(var i=0; i<removeTri.length; i++) {
+			for(var j=0; j<3; j++) {
+				pointsDupricated.push(tri[removeTri[i]][j]);
+			}
+		}
+		var points = pointsDupricated.filter(function (x, i, self) {
+			return self.indexOf(x)===i;
+		});
+
+		// removeTriリストの三角形をtriから削除する
+		for(var i=0; i<removeTri.length; ++i) {
+			tri.splice(removeTri[i], 1);
+		}
+
+		// 多角形の頂点を反時計回りに並べ替え
+		points.sort(
+			function (val1, val2) {
+				th1=Math.atan2(dPos[val1][1]-p[1], dPos[val1][0]-p[0]);
+				th2=Math.atan2(dPos[val2][1]-p[1], dPos[val2][0]-p[0]);
+				return th2-th1;
+			}
+		);
+
+		// 入力点と多角形の辺で構成される三角形を追加
+		for(var i=0; i<points.length; i++) {
+			var newTri=[points[i], points[(i+1)%points.length], dPos.length-1];
+			tri.push(newTri);
+		}
+	}
+
+	// superTriangleの頂点(0,1,2番)を含む三角形を探して削除
+	removeTri = [];
+	for(var i=0; i<tri.length; ++i) {
+		for(var j=0; j<3; ++j) {
+			if(tri[i][j]==0||tri[i][j]==1||tri[i][j]==2) {
+				removeTri.unshift(i);
+				break;
+			}
+		}
+	}
+	for(var i=0; i<removeTri.length; ++i) {
+		tri.splice(removeTri[i],1);
+	}
+
+	// superTriangleの頂点を削除してtriに格納されているインデックスを-3する
+	// dPos.splice(0, 3); とすると dPos == inputPoints となる
+	for(var i=0; i<tri.length; ++i) {
+		for(var j=0; j<3; ++j) {
+			tri[i][j]-=3;
+		}
+	}
+
+	return tri;
+}
+
+
+// 外接円クラス
+function Circumcircle(p1, p2, p3) {
+	this.p1=numeric.clone(p1);
+	this.p2=numeric.clone(p2);
+	this.p3=numeric.clone(p3);
+	this.a=this.len(this.p2, this.p3);
+	this.b=this.len(this.p3, this.p1);
+	this.c=this.len(this.p1, this.p2);
+	this.s=(this.a+this.b+this.c)*0.5;
+	this.S=this.calcTriArea();
+	this.rad=this.calcRad();
+	this.p=this.calcCenter();
+}
+
+Circumcircle.prototype.len=function (p1, p2) {
+	var r=numeric.sub(p1, p2);
+	var len=numeric.norm2(r);
+	return len;
+}
+
+Circumcircle.prototype.calcTriArea=function () {
+	var area=Math.sqrt(this.s*(this.s-this.a)*(this.s-this.b)*(this.s-this.c));
+	return area;
+}
+
+Circumcircle.prototype.calcRad=function () {
+	return (this.a*this.b*this.c)/(4.0*this.S);
+}
+
+Circumcircle.prototype.calcCenter=function () {
+	var tmp1=this.a*this.a*(this.b*this.b+this.c*this.c-this.a*this.a);
+	var tmpv1=numeric.mul(tmp1, this.p1);
+
+	var tmp2=this.b*this.b*(this.c*this.c+this.a*this.a-this.b*this.b);
+	var tmpv2=numeric.mul(tmp2, this.p2);
+
+	var tmp3=this.c*this.c*(this.a*this.a+this.b*this.b-this.c*this.c);
+	var tmpv3=numeric.mul(tmp3, this.p3);
+
+	var p;
+	p=numeric.add(tmpv1, tmpv2);
+	p=numeric.add(p, tmpv3);
+	p=numeric.div(p, 16*this.S*this.S);
+	return p;
+}
---
+++ js/main.js
@@ -0,0 +1,395 @@
+// JavaScript Document
+/// <reference path="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" />
+/// <reference path="numeric-1.2.6.min.js" />
+
+var colors=['lightsalmon', 'lightseagreen', 'antiquewhite', 'aquamarine', 'beige', 'burlywood', 'mistyrose', 'mediumpurple', 'darkcyan', 'darkgray', 'orchid', 'peru', 'dodgerblue'];
+var N=10;
+var distMin=20;
+
+$(document).ready(function () {
+	initEvents($("#myCanvas"));
+});
+
+// キャンバスにイベントを紐付ける関数
+function initEvents(canvas) {
+	var canvasWidth=canvas.width();
+	var canvasHeight=canvas.height();
+	var clickPoint=[];
+
+	var points=[];	// 頂点の座標群
+	var tri=[];		// 頂点のコネクティビティ
+	var head=[];	// DelauneyTriangleクラスのインスタンス配列
+	init();
+
+	var selectPoint=null;
+	var clickState="up";
+	draw();
+	// mouseクリック時のイベントコールバック設定
+	canvas.mousedown(function (event) {
+		// 左クリック
+		if(event.button==0) {
+			var canvasOffset=canvas.offset();
+			var canvasX=Math.floor(event.pageX-canvasOffset.left);
+			var canvasY=Math.floor(event.pageY-canvasOffset.top);
+			if(canvasX<0||canvasX>canvasWidth) {
+				return;
+			}
+			if(canvasY<0||canvasY>canvasHeight) {
+				return;
+			}
+			clickState="down";
+			clickPoint = [canvasX, canvasY];
+			draw();
+		}
+	});
+
+	// mouse移動時のイベントコールバック設定
+	canvas.mousemove(function (event) {
+		var canvasOffset=canvas.offset();
+		var canvasX=Math.floor(event.pageX-canvasOffset.left);
+		var canvasY=Math.floor(event.pageY-canvasOffset.top);
+		if(canvasX<0||canvasX>canvasWidth) {
+			return;
+		}
+		if(canvasY<0||canvasY>canvasHeight) {
+			return;
+		}
+		if(clickState=="down") {
+			clickPoint=[canvasX, canvasY];
+			draw();
+		}
+	});
+	// mouseクリック解除時のイベントコールバック設定
+	$(window).mouseup(function (event) {
+		clickState="up";
+	});
+
+	$("input").click(function() {draw()});
+
+	// リセットボタン
+	$("#reset").click(function () {
+		init();
+		draw();
+	})
+
+	
+	// 点群の生成と三角形分割
+	function init() {
+		// ランダムな点群を生成
+		points = generateRondomPoints(canvasWidth, canvasHeight, distMin, N);
+		// 三角形分割
+		tri=new DelaunayTriangulation(points, canvasHeight, 0, canvasWidth, 0);
+		// DelauneyTriangleオブジェクトの連結リストを作成
+		head = makeDelauneyTriangleList(points, tri);
+		// DelauneyTriangleオブジェクトの隣接関係を作成
+		makeDataStructureForTriangles(head);
+	}
+
+
+	// レンダリングのリフレッシュを行う関数
+	function draw() {
+
+
+		// ローソンの三角形探査
+		if(clickPoint.length != 0) {
+			var resultTri = lawsonTriangleDetection(points, head, clickPoint);
+			points.push(clickPoint);
+			resultTri.deleteAndAdd(points.length-1, points);
+		}
+		
+		var context = canvas.get(0).getContext("2d");
+		context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+		// 三角形の描画
+		context.strokeStyle='black';
+		if($('#colorCheckBox').is(':checked')) {
+			fillTrianglesFromHead(canvas, points, head);
+		}
+		drawTrianglesFromHead(canvas, points, head);
+
+		// 外接円の描画
+		if($('#gaisetuenCheckBox').is(':checked')) {
+			drawCircumcircles(canvas, points, tri);
+		}
+		context.fillStyle='black';
+
+		// 点の描画
+		drawPoints(canvas, points);
+
+
+		// ローソンのアルゴリズムの過程で通った三角形の描画
+		//context.fillStyle='orange';
+		//drawTriangle(resultTri.vertexID);
+
+
+		// 出発三角形の描画
+		//context.fillStyle='green';
+		//drawTriangle(head.vertexID);
+
+
+		// クリックした点の描画
+		context.fillStyle='red';
+		context.beginPath();
+		context.arc(clickPoint[0], clickPoint[1], 5, 0, Math.PI*2, true);
+		context.fill();
+
+		function drawTriangle(vertexID) {
+			context.beginPath();
+			context.moveTo(points[vertexID[0]][0], points[vertexID[0]][1]);
+			context.lineTo(points[vertexID[1]][0], points[vertexID[1]][1]);
+			context.lineTo(points[vertexID[2]][0], points[vertexID[2]][1]);
+			context.lineTo(points[vertexID[0]][0], points[vertexID[0]][1]);
+			context.fill();
+			context.stroke();
+		}
+
+	}
+}
+
+function lawsonTriangleDetection(points, head, newPoint) {
+	// head から順にローソンのアルゴリズムを適用していく
+	var triTmp = head;
+	var edge = 0;
+	var vEdge, vPt;
+	var isPointInner = false;
+	var edgeTmp;
+	while(1) {
+		isPointInner = true;
+		for(var i = 0; i < 3; ++i) {
+			vPt = numeric.sub(newPoint, points[triTmp.vertexID[edge]]);
+			vEdge = numeric.sub(points[triTmp.vertexID[(edge + 1) % 3]], points[triTmp.vertexID[edge]]);
+			// newPointが辺ベクトルの右側にあれば右隣りの三角形に移る
+			if(vPt[0] * vEdge[1] - vPt[1] * vEdge[0] > 0) {
+				edgeTmp = triTmp.edgeIDinAdjacent[edge];
+				triTmp = triTmp.adjacent[edge];
+				edge = (edgeTmp + 1) % 3
+				isPointInner = false;
+				if(triTmp == null) {
+					alert("Triangle search failed.");
+				}
+				break;
+			}
+			edge = (edge + 1) % 3;
+		}
+		if(isPointInner) {
+			return triTmp;
+		}
+	}
+}
+		
+function lawsonTriangleDetectionPath(points, head, newPoint) {
+	// head から順にローソンのアルゴリズムを適用していく
+	var triTmp = head;
+	var triAndEdge = [];
+	var edge = 0;
+	var triPath = [];
+	var vEdge, vPt;
+	var isPointInner = false;
+	var edgeTmp;
+	while(1) {
+		isPointInner = true;
+		for(var i = 0; i < 3; ++i) {
+			triAndEdge.push({ triangle: triTmp, edge: edge });
+			vPt = numeric.sub(newPoint, points[triTmp.vertexID[edge]]);
+			vEdge = numeric.sub(points[triTmp.vertexID[(edge + 1) % 3]], points[triTmp.vertexID[edge]]);
+			// newPointが辺ベクトルの右側にあれば右隣りの三角形に移る
+			if(vPt[0] * vEdge[1] - vPt[1] * vEdge[0] > 0) {
+				triPath.push(triTmp);
+				edgeTmp = triTmp.edgeIDinAdjacent[edge];
+				triTmp = triTmp.adjacent[edge];
+				edge = (edgeTmp + 1) % 3
+				isPointInner = false;
+				if(triTmp == null) {
+					isPointInner = true;
+					alert("Triangle search failed.");
+				}
+				break;
+			}
+			edge = (edge + 1) % 3;
+		}
+		if(isPointInner) {
+			triPath.push(triTmp);
+			break;
+		}
+	}
+	return {trianglePath:triPath, triangleAndEdgePath:triAndEdge};
+}
+
+// DelauneyTriangleの連結リストを生成する
+// 連結リストの先頭を返す
+function makeDelauneyTriangleList(points, tri) {
+	var head=new DelauneyTriangle(points, tri[0]);
+	var dTriTmp=head;
+	for(var i=1; i<tri.length; ++i) {
+		dTriTmp.push(new DelauneyTriangle(points, tri[i]));
+		dTriTmp=dTriTmp.next;
+	}
+	return head;
+}
+
+
+// ランダムな点群を生成する
+function generateRondomPoints(width, height, distMin, N){
+	var randx, randy;
+	var dist, isTooClose;
+	var points=[];
+	for(var i=0; i<N; ++i) {
+		for(var j=0; j<10; ++j) {
+			randx=0.8*width*(Math.random()-0.5)+0.5*width;
+			randy=0.8*height*(Math.random()-0.5)+0.5*height;
+			isTooClose=false;
+			for(var j=0; j<points.length; ++j) {
+				dist=(randx-points[j][0])*(randx-points[j][0])+(randy-points[j][1])*(randy-points[j][1]);
+				if(dist<distMin*distMin) {
+					isTooClose=true;
+					break;
+				}
+			}
+			if(!isTooClose) {
+				points.push([randx, randy]);
+				break;
+			}
+		}
+	}
+	return points;
+}
+
+// headを先頭とする連結リストとして格納された
+// DelauneyTriangleオブジェクトのadjacent, edgeIDinAdjacentを作成する
+function makeDataStructureForTriangles(head){
+	// dTri.adjacentを探索する
+	// すべての辺について総当たりで調べる
+	var foundFlag;
+	var focusTri=null;
+	var compTri=null;
+	for(focusTri=head; focusTri!=null; focusTri=focusTri.next) {
+		for(var j=0; j<3; ++j) {
+			// すでに隣接する辺が見つかっていれば飛ばす
+			if(focusTri.adjacent[j]!=null) {
+				continue;
+			}
+			// すべての辺と総当たりで照合する
+			foundFlag=false;
+			for(compTri=head; compTri!=null; compTri=compTri.next){
+				for(var l=0; l<3; ++l) {
+					if(
+						focusTri.vertexID[j]==compTri.vertexID[(l+1)%3]
+						&&
+						focusTri.vertexID[(j+1)%3]==compTri.vertexID[l]
+						) {
+						focusTri.adjacent[j]=compTri;
+						focusTri.edgeIDinAdjacent[j]=l;
+						compTri.adjacent[l]=focusTri;
+						compTri.edgeIDinAdjacent[l]=j;
+						foundFlag=true;
+						break;
+					}
+				}
+				if(foundFlag) {
+					break;
+				}
+			}
+		}
+	}
+
+}
+
+
+// 以下、描画関係
+
+function drawPoints(canvas, points) {
+	var context=canvas.get(0).getContext("2d");
+	var canvasWidth=canvas.width();
+	var canvasHeight=canvas.height();
+	context.setTransform(1, 0, 0, 1, 0, 0);
+	for(var i=0; i<points.length; ++i) {
+		context.beginPath();
+		context.arc(points[i][0], points[i][1], 3, 0, 2*Math.PI, true);
+		context.fill();
+	}
+}
+
+function drawTriangles(canvas, points, triangles) {
+	var context=canvas.get(0).getContext("2d");
+	var canvasWidth=canvas.width();
+	var canvasHeight=canvas.height();
+	context.setTransform(1, 0, 0, 1, 0, 0);
+	var tri;
+	for(var i=0; i<triangles.length; ++i) {
+		tri=triangles[i];
+		context.beginPath();
+		context.moveTo(points[tri[0]][0], points[tri[0]][1]);
+		context.lineTo(points[tri[1]][0], points[tri[1]][1]);
+		context.lineTo(points[tri[2]][0], points[tri[2]][1]);
+		context.lineTo(points[tri[0]][0], points[tri[0]][1]);
+		context.stroke();
+	}
+}
+
+
+function drawTrianglesFromHead(canvas, points, head) {
+	var context=canvas.get(0).getContext("2d");
+	var canvasWidth=canvas.width();
+	var canvasHeight=canvas.height();
+	context.setTransform(1, 0, 0, 1, 0, 0);
+	for(var tri=head; tri!=null; tri=tri.next) {
+		context.beginPath();
+		context.moveTo(points[tri.vertexID[0]][0], points[tri.vertexID[0]][1]);
+		context.lineTo(points[tri.vertexID[1]][0], points[tri.vertexID[1]][1]);
+		context.lineTo(points[tri.vertexID[2]][0], points[tri.vertexID[2]][1]);
+		context.lineTo(points[tri.vertexID[0]][0], points[tri.vertexID[0]][1]);
+		context.stroke();
+	}
+}
+
+function fillTrianglesFromHead(canvas, points, head) {
+	var context=canvas.get(0).getContext("2d");
+	var canvasWidth=canvas.width();
+	var canvasHeight=canvas.height();
+	context.setTransform(1, 0, 0, 1, 0, 0);
+	var i=0;
+	for(var tri=head; tri!=null; tri=tri.next) {
+		context.fillStyle=colors[i%colors.length];
+		context.beginPath();
+		context.moveTo(points[tri.vertexID[0]][0], points[tri.vertexID[0]][1]);
+		context.lineTo(points[tri.vertexID[1]][0], points[tri.vertexID[1]][1]);
+		context.lineTo(points[tri.vertexID[2]][0], points[tri.vertexID[2]][1]);
+		context.lineTo(points[tri.vertexID[0]][0], points[tri.vertexID[0]][1]);
+		context.fill();
+		++i;
+	}
+}
+
+
+function fillTriangles(canvas, points, triangles) {
+	var context=canvas.get(0).getContext("2d");
+	var canvasWidth=canvas.width();
+	var canvasHeight=canvas.height();
+	context.setTransform(1, 0, 0, 1, 0, 0);
+	var tri;
+	for(var i=0; i<triangles.length; ++i) {
+		context.fillStyle=colors[i%colors.length];
+		tri=triangles[i];
+		context.beginPath();
+		context.moveTo(points[tri[0]][0], points[tri[0]][1]);
+		context.lineTo(points[tri[1]][0], points[tri[1]][1]);
+		context.lineTo(points[tri[2]][0], points[tri[2]][1]);
+		context.lineTo(points[tri[0]][0], points[tri[0]][1]);
+		context.fill();
+	}
+}
+
+
+// 三角形の外接円を描画
+function drawCircumcircles(canvas, points, tri) {
+	var context=canvas.get(0).getContext("2d");
+	var cir;
+	for(var i=0; i<tri.length; ++i) {
+		context.strokeStyle=colors[i%colors.length];
+		context.beginPath();
+		cir=new Circumcircle(points[tri[i][0]], points[tri[i][1]], points[tri[i][2]]);
+		context.arc(cir.p[0], cir.p[1], cir.rad, 0, Math.PI*2, true);
+		context.stroke();
+	}
+}
+
